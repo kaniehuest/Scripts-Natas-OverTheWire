482734ec29b2a629211020e9d08887d674abcfcb
kaniehuest <carlos.ojeda.6556@gmail.com>
2022-12-29 18:28:51 -0300

    Agregado nivel 1 de natas

--- Natas-0-1.py
+++ Natas-0-1.py
@@ -3,12 +3,12 @@ import requests
 
 
 def main():
-    URL = "http://natas0.natas.labs.overthewire.org"
+    URL = "http://natas1.natas.labs.overthewire.org"
     s = requests.Session()
-    s.auth = ("natas0", "natas0")
+    s.auth = ("natas1", "g9D9cREhslqBKtcA2uocGHPfMZVzeFK6")
     r = s.get(URL)
     natas1_password = re.findall(r"([a-zA-Z\d]{32})", r.text) 
-    print("[!] The password for Natas 1 is: " + natas1_password[0])
+    print("[!] The password for Natas 2 is: " + natas1_password[1])
 
 
 if __name__ == '__main__':
